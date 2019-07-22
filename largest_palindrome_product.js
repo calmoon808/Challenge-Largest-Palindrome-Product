899d805de367c18536183f33d8e7b409496b70aa
Calvin <calmoon808@gmail.com>
2019-07-21 14:54:34 -1000

    can cover all numbers

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -11,46 +11,36 @@ module.exports = function(digits){
   var palindromeNumber = 0;
 
   // do your work here
-  if (digits === 2){
-    for (let i = 10; i < 100; i++){
-      for (let j = 10; j < 100; j++){
-        let product = i * j;
-        let compareNum = Array.from(product.toString()).map(Number);
-        if (compareNum[0] === compareNum[3] && compareNum[1] === compareNum[2]){
-          compareNum = compareNum.join('') * 1;
-          factor_0 = i;
-          factor_1 = j;
-          palindromeNumber = compareNum; 
+  let num = Math.pow(10, digits);
+  for (let i = 0; i < num; i++){
+    for (let j = 0; j < num; j++){
+      let product = i * j;
+      let compareNum = Array.from(product.toString()).map(Number);
+      let limit = Math.floor(compareNum.length / 2);
+      let count = 0
+      let isSame = false;
+      while (count !== limit){
+        if (compareNum[count] === compareNum[compareNum.length - (count + 1)]){
+          isSame = true;
+          count++;
+        } else {
+          isSame = false;
+          count = limit;
+        }
+      }
+      if (isSame){
+        compareNum = compareNum.join('') * 1;
+        factor_0 = i;
+        factor_1 = j;
+        if (compareNum > palindromeNumber){
+          palindromeNumber = compareNum;
         }
       }
     }
   }
-
-  if (digits === 3){
-    for (let i = 100; i < 999; i++){
-      for (let j = 100; j < 999; j++){
-        let product = i * j;
-        let compareNum = Array.from(product.toString()).map(Number);
-        if (compareNum[0] === compareNum[5] && 
-          compareNum[1] === compareNum[4] && 
-          compareNum[2] === compareNum[3]){
-          compareNum = compareNum.join('') * 1;
-          factor_0 = i;
-          factor_1 = j;
-          if (compareNum > palindromeNumber){
-            palindromeNumber = compareNum;
-          } 
-        }
-      }
-    }
-  }
-
-
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
 };
-
-console.log(Math.floor(4 / 2));
